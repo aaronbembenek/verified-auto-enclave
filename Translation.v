9d934ca2fd0598b414c3861fad5d8db1c1841f6b
Aaron Bembenek <aaron.bembenek@gmail.com>
2017-04-22 11:04:10 -0400

    Updated context/type translation.

--- Translation.v
+++ Translation.v
@@ -20,37 +20,41 @@ Module E := ImpE.
 
 Section TypeTrans.
 
-  Definition dom_eq (G: S.context) (G': E.context) :=
-    (forall x, (exists t, S.var_in_dom G x t) <->
-               (exists t', E.var_in_dom G' x t')) /\
-    (forall l, (exists t rt, S.loc_in_dom G l t rt) <->
-               (exists t' rt', E.loc_in_dom G' l t' rt')).
+  Definition subdom {A B C: Type} (f: A -> option B) (g: A -> option C) :=
+    forall x,
+      match f x with
+      | Some _ => exists y, g x = Some y
+      | None => True
+      end.
   
-  Inductive ttrans : S.base_type -> E.loc_mode -> E.base_type -> Prop :=
-  | Ttrans_nat : forall d, ttrans S.Tnat d E.Tnat
-  | Ttrans_cond : forall d md, ttrans S.Tcond d (E.Tcond md)
-  | Ttrans_ref : forall d s s' p md rt,
-      ttrans s d s' ->
-      ttrans (S.Tref (S.Typ s p) rt) d (E.Tref (E.Typ s' p) md rt)
-  | Ttrans_lambda : forall Gm G'm Gp G'p d U p Km Kp md,
+  Inductive btrans : S.base_type -> E.loc_mode -> E.base_type -> Prop :=
+  | Btrans_nat : forall d, btrans S.Tnat d E.Tnat
+  | Btrans_cond : forall d md, btrans S.Tcond d (E.Tcond md)
+  | Btrans_ref : forall d s s' p md rt,
+      btrans s d s' ->
+      btrans (S.Tref (S.Typ s p) rt) d (E.Tref (E.Typ s' p) md rt)
+  | Btrans_lambda : forall Gm G'm Gp G'p d U p Km Kp md,
       context_trans Gm d G'm ->
       context_trans Gp d G'p ->
-      ttrans (S.Tlambda Gm U p Gp) d (E.Tlambda G'm Km U p md G'p Kp)
+      btrans (S.Tlambda Gm U p Gp) d (E.Tlambda G'm Km U p md G'p Kp)
 
-  with type_trans : S.type -> E.loc_mode -> E.type -> Prop :=
+  with ttrans : S.type -> E.loc_mode -> E.type -> Prop :=
   | Ttrans : forall s p s' d,
-      ttrans s d s' ->
-      type_trans (S.Typ s p) d (E.Typ s' p)
+      btrans s d s' ->
+      ttrans (S.Typ s p) d (E.Typ s' p)
 
   with context_trans : S.context -> E.loc_mode -> E.context -> Prop :=
   | Gtrans : forall G d G',
-      dom_eq G G' ->
+      subdom (S.var_context G) (E.var_context G') ->
+      subdom (S.loc_context G) (E.loc_context G') ->
+      subdom (E.var_context G') (S.var_context G) ->
+      subdom (E.loc_context G') (S.loc_context G) ->
       S.forall_var G (fun x t =>
                         exists t',
-                          type_trans t d t' /\ E.var_in_dom G' x t') ->
+                          ttrans t d t' /\ E.var_in_dom G' x t') ->
       S.forall_loc G (fun x t rt =>
                         exists t',
-                          type_trans t d t' /\ E.loc_in_dom G' x t' rt) ->
+                          ttrans t d t' /\ E.loc_in_dom G' x t' rt) ->
       S.forall_loc G (fun x t rt =>
                         let (s, p) := t in
                         policy_le p (LevelP L) \/ d x <> E.Normal) ->
